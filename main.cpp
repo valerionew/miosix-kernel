12ce8525c10cd3bde95da1f14c35d9c5c01b299b
valerionew <valerionappi991@gmail.com>
2022-09-04 19:48:47 +0200

    dma working with threads

--- main.cpp
+++ main.cpp
@@ -1,36 +1,24 @@
-
 #include <cstdio>
 #include "miosix.h"
 #include "WS2812.h"
 #include <RGB.h>
+#include <kernel/scheduler/scheduler.h>
+#include <interfaces/delays.h>
+
 
 #define numleds 60
 
 using namespace miosix;
+
+static Thread *waiting;
+
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
 
 using sck  = Gpio<GPIOB_BASE,13>; //Used as HW SPI
 using mosi = Gpio<GPIOB_BASE,15>; //Used as HW SPI
 using miso = Gpio<GPIOB_BASE,14>; //Used as HW SPI
 using sig = Gpio<GPIOB_BASE,3>; 
 
-void spi_init(){
-    { 
-        sck::mode(Mode::ALTERNATE);   
-        sck::alternateFunction(5);
-        mosi::mode(Mode::ALTERNATE); 
-        mosi::alternateFunction(5);
-        miso::mode(Mode::ALTERNATE); 
-        miso::alternateFunction(5);
-        RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
-        RCC_SYNC();
-    }
-
-        SPI2->CR1=SPI_CR1_SSM  //No HW cs
-                | SPI_CR1_SSI
-                | SPI_CR1_BR_1 //fclk/4 works fine
-                | SPI_CR1_SPE  //SPI enable 
-                | SPI_CR1_MSTR;//Master mode
-}
 
 void spi_dma_init(){
         // using DMA1 channel 4 and SPI2
@@ -49,28 +37,16 @@ void spi_dma_init(){
         SPI2->CR2=SPI_CR2_TXDMAEN;
         SPI2->CR1=SPI_CR1_SSM  //software slave management
                 | SPI_CR1_SSI  //software slave set
-                | SPI_CR1_BR_1 //fclk/4 works fine
+                | SPI_CR1_BR_0 //fclk/4 works fine
                 | SPI_CR1_SPE  //SPI enable 
                 | SPI_CR1_MSTR;//Master mode
 
 }
 
-void spi_transmit(uint8_t * data, const uint32_t length){  
-    // do not interrupt
-    // particularly because we are using polling for 
-    // the SPI and the timing  between the bytes are critical
-    FastInterruptDisableLock dLock;
-
-    uint8_t*pointer = data;
-    uint32_t remaining = length;
-    while(remaining-- > 0){
-        while((SPI2->SR & SPI_SR_TXE)==0){} // wait for tx buffer to be empty
-        SPI2->DR=*pointer++; //send data
-    }
-    while(SPI2->SR & SPI_SR_BSY){} // wait for send complete
-}
-
 void spi_transmit_dma(uint8_t * data, uint32_t length){
+
+        pthread_mutex_lock(&mutex);
+        waiting=Thread::getCurrentThread();
 
         NVIC_ClearPendingIRQ(DMA1_Stream4_IRQn);
         NVIC_SetPriority(DMA1_Stream4_IRQn,10);//Low priority for DMA
@@ -87,18 +63,32 @@ void spi_transmit_dma(uint8_t * data, uint32_t length){
                     | DMA_SxCR_TEIE    //Interrupt on transfer error
                     | DMA_SxCR_DMEIE   //Interrupt on direct mode error
                     | DMA_SxCR_EN;     // start DMA
-
-
+                        
+    {
+        FastInterruptDisableLock dLock;
+        while(waiting!=0)
+        {
+            waiting->IRQwait();
+            {
+                FastInterruptEnableLock eLock(dLock);
+                Thread::yield();
+            }
+        }
+    }
+    pthread_mutex_unlock(&mutex);
 }
 
 
 void __attribute__((naked)) DMA1_Stream4_IRQHandler()
 {
     saveContext();
-    ledOff();
         DMA1->HIFCR=DMA_HIFCR_CTCIF4
               | DMA_HIFCR_CTEIF4
               | DMA_HIFCR_CDMEIF4;
+    waiting->IRQwakeup();
+    if(waiting->IRQgetPriority()>Thread::IRQgetCurrentThread()->IRQgetPriority())
+    Scheduler::IRQfindNextThread();
+    waiting=0;
     restoreContext();
 }
 
@@ -109,8 +99,8 @@ const RGB_t<uint8_t>	green	(  0, 255,   0);
 const RGB_t<uint8_t>	yellow	(127, 255,   0);
 const RGB_t<uint8_t>	orange	(255,  50,   0);
 const RGB_t<uint8_t>	red		(255,   0,   0);
-
-
+const RGB_t<uint8_t>	white	(255, 255, 255);
+const RGB_t<uint8_t>	black	(  0,   0,   0);
 
 std::array<RGB_t<uint8_t>, 6> rainbow = {violet, blue, green, yellow, orange, red};
 
@@ -128,9 +118,9 @@ int main(){
             for(int j = 0; j < numleds; j++){
                 leds.setPixel(j, rainbow[(j+i)%6]);
             }
-        leds.show();
-        Thread::sleep(50);
-               }
+            leds.show();
+            Thread::sleep(50);
+        }
     }
 }
 
